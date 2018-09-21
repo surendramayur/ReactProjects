468c5734695bda027f9d572a0ccf9dc8a2c61b21
Mayur <surendramayur@gmail.com>
2018-09-21 18:14:26 +0530

    passing method references bw components with bind and arrow func

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('was clicked!');
     //Don't Do THIS: this.state.persons[0].name = 'Mayur K';
     this.setState( {
       persons: [
-        {name: 'Mayur K', age: 28},
+        {name: newName, age: 28},
         {name: 'May', age: 35},
         {name: 'Mann', age: 25}
       ]
@@ -31,10 +31,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>Well this is working</p>
-        <button onClick={this.switchNameHandler} >Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Reading</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Mayur Kr')} >Switch Name</button>
+        <Person 
+        name={this.state.persons[0].name} 
+        age={this.state.persons[0].age} />
+        <Person 
+        name={this.state.persons[1].name} 
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Mayur K')} >My Hobbies: Reading</Person>
+        <Person 
+        name={this.state.persons[2].name} 
+        age={this.state.persons[2].age} />
       </div>      
     );
   
