4b0f659d1d127868701c3d90f16cdff0db47943f
Mayur <surendramayur@gmail.com>
2018-09-22 13:25:57 +0530

    making lists flexible to update individually

--- src/App.js
+++ src/App.js
@@ -15,14 +15,25 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState( {
-      persons: [
-        {name: 'Mayur', age: 28},
-        {name: event.target.value, age: 35},
-        {name: 'Max', age: 22}
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    //alternative
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
+    this.setState( {persons: persons});
   }
 
   deletePersonHandler = (personIndex) => {
@@ -57,7 +68,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)} 
               name={person.name} 
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
