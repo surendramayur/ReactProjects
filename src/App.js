ddf17f1d896986a0e48de75b0983e0a7c5b5fd32
Mayur <surendramayur@gmail.com>
2018-09-23 09:26:25 +0530

    setting button styles dynamically

--- src/App.js
+++ src/App.js
@@ -51,7 +51,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -73,6 +74,8 @@ class App extends Component {
           })}
         </div>
       );
+
+      style.backgroundColor = 'red';
     }
 
     return (
