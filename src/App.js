2f3d7f331816364fc90f546afe83db16fac93e8f
Mayur <surendramayur@gmail.com>
2018-09-23 12:51:19 +0530

    using pseudo selector n media query with css modules after npm eject

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 //import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
@@ -50,17 +50,8 @@ class App extends Component {
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'      
-    }
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -74,26 +65,26 @@ class App extends Component {
               changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
-      );
-
-      style.backgroundColor = 'red';      
+      ); 
+      
+      btnClass = classes.Red;
     }
 
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App} >
           <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')} >Well this is working</p>
+          <p className={assignedClasses.join(' ')} >Well this is working</p>
           <button 
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonsHandler} >Toggle Persons</button>
           {persons}
         </div>
