d2d08158a569bb13cc7f01955d5389b46cd401b7
Mayur <surendramayur@gmail.com>
2018-09-24 17:49:24 +0530

    more with ref api react 16.3 and higher

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -6,7 +6,8 @@ class Persons extends PureComponent {
 
     constructor(props) {
         super(props);
-        console.log('[Persons.js] Inside Constructor', props);    
+        console.log('[Persons.js] Inside Constructor', props); 
+        this.lastPersonRef = React.createRef();   
     }
 
     componentWillMount() {
@@ -15,6 +16,7 @@ class Persons extends PureComponent {
 
     componentDidMount () {
         console.log('[Persons.js] Inside componentDidMount');
+        this.lastPersonRef.current.focus();
     }
 
     componentWillReceiveProps (nextProps) {
@@ -46,6 +48,7 @@ class Persons extends PureComponent {
                 name={person.name}
                 position={index} 
                 age={person.age}
+                ref={this.lastPersonRef}
                 key={person.id}
                 changed={(event) => this.props.changed(event, person.id)} />;
         });
