a36bae1bf681eb4b7159ca1fef219746cd0abc16
Mayur <surendramayur@gmail.com>
2018-09-24 17:30:11 +0530

    alternate createRef() for React 16.3 and higher for using ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -9,6 +9,7 @@ class Person extends Component {
     constructor(props) {
         super(props);
         console.log('[Person.js] Inside Constructor', props);
+        this.inputElement = React.createRef();
     }
 
     componentWillMount() {
@@ -18,7 +19,7 @@ class Person extends Component {
     componentDidMount() {
         console.log('[Person.js] Inside componentDidMount');
         if (this.props.position === 0) {
-            this.inputElement.focus();
+            this.inputElement.current.focus();
         }        
     }
 
@@ -29,7 +30,7 @@ class Person extends Component {
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input 
-                    ref={(inp) => { this.inputElement = inp }}
+                    ref={this.inputElement}
                     type="text" 
                     onChange={this.props.changed} 
                     value={this.props.name} />
